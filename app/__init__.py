3eda2c58ba1f32790d22189802e3c620a224c910
June Muoti <june.muoti@gmail.com>
2018-02-05 00:24:37 +0300

    add

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap(app)
 #initialize app
+def create_app(config_name):
+    app = Flask(__name__)
 
+    app.config.from_object(config_options[config_name])
 
-app = Flask(__name__)
+    bootstrap.init_app(app)
 
-from app import views
+    return app
+
