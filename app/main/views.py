db8f78b8379b7ee0a16f08eeb1c5a46c3248ae6f
June Muoti <june.muoti@gmail.com>
2018-02-05 23:15:07 +0300

    add styling

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,17 @@
 from flask import render_template
 from . import main
 from flask_login import login_required,login_user,logout_user
+from ..models import Category
 
 
 @main.route('/')
 def index():
+    title = 'Home'
 
-    return render_template('index.html')
+    categories = Category.get_categories()
 
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("main.index"))
+    return render_template('index.html' ,title=title, categories=categories)
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin,db.Model):
     __tablename__= 'users'
     id=db.Column(db.Integer,primary_key =True)
     username = db.Column(db.String(255))
-    email=db.Column(db.String(255),unique=True index = True)
+    email=db.Column(db.String(255),unique=True,index = True)
     pass_hash =db.Column(db.String(255))
 
     @property 
@@ -25,8 +25,34 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
 
-
-
     def __repr__(self):
 
         return f'User {self.username}'
+
+class Category(db.Model):
+    __tablename__ ='categories'
+    id = db.Column(db.Integer,primary_key= True)
+    name=db.Column(db.String(255))
+
+    @classmethod
+    def get_categories(cls):
+        '''
+        Function that queries the Groups Table in the database and returns all the information from the Groups Table
+        Returns:
+            groups : all the information in the groups table
+        '''
+
+        categories = Category.query.all()
+
+        return categories
+
+        
+class Pitch(db.Model):
+    __tablename__='pitches'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(255))
+
+    
+
+
+    
