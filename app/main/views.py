dfbf84e104ee37c4bc31f4569f531f9832fe859f
June Muoti <june.muoti@gmail.com>
2018-02-05 16:39:44 +0300

    add register

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,15 @@
 from flask import render_template
 from . import main
+from flask_login import login_required,login_user,logout_user
 
 
 @main.route('/')
 def index():
 
     return render_template('index.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
