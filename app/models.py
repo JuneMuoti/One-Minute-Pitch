cabea22af28d91199aa2fd16d91e26e7ffd0c393
June Muoti <june.muoti@gmail.com>
2018-02-09 11:55:19 +0300

    add relations

--- app/models.py
+++ app/models.py
@@ -16,61 +16,63 @@ class User(UserMixin,db.Model):
     email=db.Column(db.String(255),unique=True,index = True)
     pass_hash =db.Column(db.String(255))
 
-    @property 
+    @property
     def password(self):
         raise AttributeError('You cannot read the password Attribute')
-        
+
     @password.setter
     def password(self,password):
-        self.pass_secure =generate_password_hash(password)
+        self.pass_hash =generate_password_hash(password)
     def verify_password(self,password):
         return check_password_hash(self.pass_hash,password)
 
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
 
-        return f'User {self.username}'
+         return f'User {self.username}'
 
-class Category(db.Model):
-    __tablename__ ='categories'
+class Comments(db.Model):
+    __tablename__ ='comments'
     id = db.Column(db.Integer,primary_key= True)
     name=db.Column(db.String(255))
-    pitches=db.relationship('Pitch',backref='category',lazy='dynamic')
+    pitches=db.relationship('Pitch',backref='comments',lazy='dynamic')
 
-    def save_category(self):
+    def save_comment(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_categories(cls):
+    def get_comments(cls):
         '''
         Function that queries the Groups Table in the database and returns all the information from the Groups Table
         Returns:
             groups : all the information in the groups table
         '''
 
-        categories = Category.query.all()
+        comments = Comments.query.all()
 
-        return categories
+        return comments
 
-        
+
 class Pitch(db.Model):
     __tablename__='pitches'
     id=db.Column(db.Integer,primary_key=True)
-    pitch_line=db.Column(db.String(255))
-    category_id =db.Column(db.Integer,db.ForeignKey("categories.id"))
-    user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+    title=db.Column(db.String(255))
+    description=db.Column(db.String)
+    comments_id =db.Column(db.Integer,db.ForeignKey("comments.id"))
+
 
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_pitches(cls,category_id):
+    def get_pitches(cls):
         '''
-        gets pitches of a selected category from the database
+        gets pitches from the database
         '''
-        pitches=Pitch.query.order_by(Pitch.id.desc()).filter_by(category_id=category_id).all()
+        pitches=Pitch.query.all()
         return pitches
-
-
-    
