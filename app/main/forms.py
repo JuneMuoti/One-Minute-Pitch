ff1eecf7042ec4658de8a817a9c2c1ac22d711a5
June Muoti <june.muoti@gmail.com>
2018-02-09 11:45:51 +0300

    added pitch and comment forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+    title= StringField('Enter the pitch category ',validators = [Required()])
+    description= StringField('Enter the pitch description' ,validators = [Required()])
+
+    submit = SubmitField('create')
+
+class CommentForm(FlaskForm):
+
+    name= StringField('Enter your comment here' ,validators = [Required()])
+
+    submit = SubmitField('create')
